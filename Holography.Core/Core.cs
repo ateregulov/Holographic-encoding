052482e8cd64e41e689e7a5b94c06f75dc7230d9
Artur Teregulov <sliceice@gmail.com>
2013-03-24 23:22:44 +0400

    .added assembly for core

---
+++ Holography.Core/Core.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace Holography.Core
+{
+    public class Core
+    {
+		BitmapData ReadFileToBitmapData(string fileName)
+		{
+			Bitmap source = new Bitmap(fileName, true);
+			BitmapData bitmapData = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+			return bitmapData;
+		}
+
+
+
+    }
+}
---
+++ Holography.Core/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holography.Core
+{
+	class Settings
+	{
+		public double w = 0.03; //длина волны источника света
+		public double z = 50; //расстояние между пластинами
+		public double sensitivityHologram = 50; //чувствительность голограммы
+		public double sensitivityOutput = 0.01; //чувствительность выходной пластины
+		public int p = -150; //смещение исходной пластинки
+		public string sourceFile = @"C:\source.bmp";
+		public string destinationFile = @"C:\hologram.bmp";
+		public int destinationWidth = 300;
+		public int destinationHeight = 300;
+	}
+}
